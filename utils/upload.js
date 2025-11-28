63046f1a3c2da481e543c6cde244f09aed35cbd6
pankaj-deepnapsoftech <itsybizz@gmail.com>
2025-11-28 12:35:51 +0530

    sdf

--- utils/upload.js
+++ utils/upload.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const path = require("path");
 
 const uploadDir = path.join(__dirname, "../tmp", "uploads");
-if (fs.existsSync(uploadDir)) {
+if (!fs.existsSync(uploadDir)) {
   fs.mkdirSync(uploadDir, { recursive: true });
 }
 
